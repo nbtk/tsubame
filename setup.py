25d11f0b9dea6d82392604aedd305a23936392c8
nbtk <nobutaka@horie.dev>
2024-08-22 18:04:36 +0900

    The version number for the package was updated.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='tsubame',
-    version='0.8.1',
+    version='0.8.2',
     description='Quick Traceroute',
     long_description=long_description,
     long_description_content_type='text/markdown',
